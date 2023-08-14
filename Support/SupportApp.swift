001f5d2f666cac1587564651f8ca7b54d8e56c47
gibonifacio <gibonifacio3@gmail.com>
2023-08-14 15:02:55 -0300

    Initial Commit

---
+++ Support/SupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupportApp.swift
+//  Support
+//
+//  Created by Giovanna Bonifacho on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
