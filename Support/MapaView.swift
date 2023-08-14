c2888f9ee1e63de2e96cd8d046217a4ab7e42d8b
gibonifacio <gibonifacio3@gmail.com>
2023-08-14 16:59:01 -0300

    Gi: atividade view concluída, perfil em andamento e mapa view criada

---
+++ Support/MapaView.swift
@@ -0,0 +1,17 @@
+//
+//  MapaView.swift
+//  Support
+//
+//  Created by Giovanna Bonifacho on 14/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct MapaView: View {
+    var body: some View {
+        VStack {
+            Text("Mapa")
+        }
+    }
+}
